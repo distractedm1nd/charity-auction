8a938886739e78a3e9ba6341232d394a536af5a2
Ryan Ford <ryanford@poluglottos.com>
2021-06-04 19:53:21 +0200

    abstracting for multiple implementations

--- contracts/CharityAuction.sol
+++ contracts/CharityAuction.sol
@@ -7,23 +7,40 @@ contract CharityAuction {
 
     address payable public charityAddress;
 
+    // Warning: Instantiating this contract and not it's inheritants will result in locked funds.
     constructor(string memory _initialMessage, address _charityAddress) public {
         message = _initialMessage;
         lastDonation = 0;
         charityAddress = payable(_charityAddress);
     }
 
-    function changeMessage(string memory _newMessage) external payable {
-        require(msg.value > last_donation);
-        message = _newMessage;
-    }
+    function changeMessage(string memory _newMessage) external payable;
 
-    // How will this get called? 🤔
-    function donateToCharity() internal {
-        selfdestruct(charityAddress);
-    }
+    // How will this get called from oracle? 🤔
+    function donateToCharity() internal;
 
     function fallback() external payable {
         changeMessage("Last donater did not leave a message.");
+    }
+}
+
+contract CharityAuctionThreshold is CharityAuction {
+    uint threshold;
+
+    // Uhh what is standard practice for where to set line breaks?
+    constructor(string memory _initialMessage, address _charityAddress, uint _threshold) Constructor(_initialMessage, _charityAddress) public {
+        threshold = _threshold;
+    }
+
+    function changeMessage(string memory _newMessage) external payable {
+        require(msg.value > lastDonation);
+        message = _newMessage;
+        if(address(this).balance + msg.value >= threshold) {
+            donateToCharity();
+        }
+    }
+
+    function donateToCharity() internal {
+        selfdestruct(charityAddress);
     }
 }
