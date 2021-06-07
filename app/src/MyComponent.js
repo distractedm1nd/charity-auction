6277aca04445d4c4c3faf24d40304a67ffb9b47b
Ryan Ford <ryanford@poluglottos.com>
2021-06-07 18:07:59 +0200

    still broken and im ignoring drizzle because im a lazy idiot.. but rimble is cool

--- app/src/MyComponent.js
+++ app/src/MyComponent.js
@@ -1,60 +1,91 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { newContextComponents } from "@drizzle/react-components";
-import logo from "./logo.png";
-
+import { Card, EthAddress, Text, Heading, Button, Modal, Flex, Box, Input } from 'rimble-ui';
 const { AccountData, ContractData, ContractForm } = newContextComponents;
 
 export default ({ drizzle, drizzleState }) => {
   // destructure drizzle and drizzleState from props
+    let [donater, setDonater] = useState("");
+    let [donation, setDonation] = useState(0);
+    let [currentMessage, setMessage] = useState("");
+    let [newMessage, setNewMessage] = useState("");
+    let [newDonation, setNewDonation] = useState("");
+    const [isOpen, setIsOpen] = useState(false);
+
+    let {lastDonater, lastDonation, message, changeMessage} = drizzle.contracts.CharityAuctionThreshold.methods;
+
+    const donate = () => {
+        changeMessage(newMessage).call({value: newDonation});
+        updateView();
+    }
+
+    const updateView = () => {
+        lastDonater().call().then(res => setDonater(res));
+        lastDonation().call().then(res => setDonation(res));
+        message().call().then(res => setMessage(res));
+    }
+
+    const closeModal = e => {
+        e.preventDefault();
+        setIsOpen(false);
+    };
+
+    const openModal = e => {
+        e.preventDefault();
+        setIsOpen(true);
+    };
+
+    useEffect(() => {
+        updateView();
+    }, [])
+
   return (
     <div className="App">
+      <Card className="section">
+        <Heading as={"h2"}>Charity Auction</Heading>
+        <Text.p>
+            To change the string, donate a higher amount than the last donater. All proceeds go to the address the owner set as beneficiary of the contract.
+        </Text.p>
+          <Text fontWeight={"bold"}>Last Message: </Text>
+          <Text.p>{currentMessage}</Text.p>
+          <Text fontWeight={"bold"}>Last Donater: </Text>
+          <EthAddress address={donater}/>
+          <Text fontWeight={"bold"}>Last Donation: </Text>
+          <Text.p>{donation}</Text.p>
+          <Button onClick={openModal}>Donate (and change the string!)</Button>
+          <Modal isOpen={isOpen}>
+              <Card width={"420px"} p={0}>
+                  <Button.Text
+                      icononly
+                      icon={"Close"}
+                      color={"moon-gray"}
+                      position={"absolute"}
+                      top={0}
+                      right={0}
+                      mt={3}
+                      mr={3}
+                      onClick={closeModal}
+                  />
 
-      <div className="section">
-        <h2>Active Account</h2>
-        <AccountData
-          drizzle={drizzle}
-          drizzleState={drizzleState}
-          accountIndex={0}
-          units="ether"
-          precision={3}
-        />
-      </div>
+                  <Box p={4} mb={3}>
+                      <Heading.h3>Donate</Heading.h3>
+                      <Input required={true} placeholder={donation + 1} value={newDonation} onChange={(e) => setNewDonation(e.target.value)}/>
+                      <Input required={true} placeholder="New Message" value={newMessage} onChange={(e) => setNewMessage(e.target.value)} />
+                  </Box>
 
-      <div className="section">
-        <h2>Charity Auction</h2>
-        <p>
-          This shows a simple ContractData component with no arguments, along
-          with a form to set its value.
-        </p>
-        <p>
-          <strong>Last Message: </strong>
-          <ContractData
-            drizzle={drizzle}
-            drizzleState={drizzleState}
-            contract="CharityAuctionThreshold"
-            method="message"
-          />
-        </p>
-          <p>
-              <strong>Last Donater: </strong>
-              <ContractData
-                  drizzle={drizzle}
-                  drizzleState={drizzleState}
-                  contract="CharityAuctionThreshold"
-                  method="lastDonater"
-              />
-          </p>
-          <p>
-              <strong>Last Donation: </strong>
-              <ContractData
-                  drizzle={drizzle}
-                  drizzleState={drizzleState}
-                  contract="CharityAuctionThreshold"
-                  method="lastDonation"
-              />
-          </p>
-        <ContractForm drizzle={drizzle} contract="CharityAuctionThreshold" method="changeMessage" sendArgs={{value: "5000000000000000000"}} />
-      </div>
+                  <Flex
+                      px={4}
+                      py={3}
+                      borderTop={1}
+                      borderColor={"#E8E8E8"}
+                      justifyContent={"flex-end"}
+                  >
+                      <Button.Outline onClick={closeModal}>Cancel</Button.Outline>
+                      <Button ml={3} onClick={() => changeMessage(newMessage).call({value: newDonation}).then(res => console.log(res))}>Confirm</Button>
+                  </Flex>
+              </Card>
+          </Modal>
+      </Card>
     </div>
   );
 };
