aaf02e08c4c4ec79dd58ce30af49851451a8a3ce
Ryan Ford <ryanford@poluglottos.com>
2021-06-09 14:44:56 +0200

    Switching to MetaMask and refactoring

--- app/src/CharityAuctionThreshold.js
+++ app/src/CharityAuctionThreshold.js
@@ -1,15 +1,21 @@
 import React, {useEffect, useState} from "react";
-import { Card, EthAddress, Text, Heading, Button, Modal, Flex, Box, Input } from 'rimble-ui';
+import { Card, EthAddress, Text, Heading, Button, Modal, Flex, Box, Input, Progress } from 'rimble-ui';
 
 export default ({ drizzle, drizzleState }) => {
   // destructure drizzle and drizzleState from props
-    let {lastDonor, lastDonation, message, changeMessage} = drizzle.contracts.CharityAuctionThreshold.methods;
+    let {lastDonor, lastDonation, message, changeMessage, threshold, remainingWei} = drizzle.contracts.CharityAuctionThreshold.methods;
     let contract = drizzleState.contracts.CharityAuctionThreshold
 
     const [isOpen, setIsOpen] = useState(false);
     let [keyStore, setKeyStore] = useState({});
     let [newMessage, setNewMessage] = useState("");
     let [newDonation, setNewDonation] = useState("");
+
+    let thresholdValue = contract.threshold[keyStore?.thresholdDataKey]?.value;
+    let remainingWeiValue = contract.remainingWei[keyStore?.remainingWei]?.value;
+    let lastDonorValue = contract.lastDonor[keyStore?.donorDataKey]?.value;
+    let lastDonationValue = contract.lastDonation[keyStore?.donationDataKey]?.value;
+    let messageValue = contract.message[keyStore?.messageDataKey]?.value;
 
     const donate = () => {
         changeMessage.cacheSend(newMessage, {value: newDonation});
@@ -21,6 +27,8 @@ export default ({ drizzle, drizzleState }) => {
             donorDataKey: lastDonor.cacheCall(),
             donationDataKey: lastDonation.cacheCall(),
             messageDataKey: message.cacheCall(),
+            remainingWeiDataKey: remainingWei.cacheCall(),
+            thresholdDataKey: threshold.cacheCall(),
         });
     }
 
@@ -42,15 +50,16 @@ export default ({ drizzle, drizzleState }) => {
     <div className="App">
       <Card className="section">
         <Heading as={"h2"}>Charity Auction</Heading>
+        <Progress value={(thresholdValue - remainingWeiValue) / thresholdValue} style={{width: "100%"}}/>
         <Text.p>
             To change the string, donate a higher amount than the last donor. All proceeds go to the address the owner set as beneficiary of the contract.
         </Text.p>
           <Text fontWeight={"bold"}>Last Message: </Text>
-          <Text.p>{contract.message[keyStore?.messageDataKey]?.value}</Text.p>
+          <Text.p>{messageValue}</Text.p>
           <Text fontWeight={"bold"}>Last Donor: </Text>
-          <EthAddress address={contract.lastDonor[keyStore?.donorDataKey]?.value}/>
+          <EthAddress address={lastDonorValue}/>
           <Text fontWeight={"bold"}>Last Donation: </Text>
-          <Text.p>{contract.lastDonation[keyStore?.donationDataKey]?.value}</Text.p>
+          <Text.p>{lastDonationValue}</Text.p>
           <Button onClick={openModal}>Donate (and change the string!)</Button>
           <Modal isOpen={isOpen}>
               <Card width={"420px"} p={0}>
--- app/src/drizzleOptions.js
+++ app/src/drizzleOptions.js
@@ -4,7 +4,6 @@ import CharityAuctionThreshold from "./contracts/CharityAuctionThreshold.json";
 const options = {
   web3: {
     block: false,
-    customProvider: new Web3("ws://localhost:7545"),
   },
   contracts: [CharityAuctionThreshold],
 };
