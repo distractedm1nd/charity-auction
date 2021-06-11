183649b6f41ecf29f01f57d82e2d393608d96e50
Ryan Ford <ryanford@poluglottos.com>
2021-06-11 10:10:44 +0200

    More Tailwind

--- app/src/CharityAuctionThreshold.js
+++ app/src/CharityAuctionThreshold.js
@@ -1,6 +1,6 @@
 import React, {Fragment, useEffect, useState} from "react";
-import {Box, Button, Card, EthAddress, Flex, Heading, Input, Modal, Progress, Text} from 'rimble-ui';
-import {CalendarIcon, CheckIcon, ChevronDownIcon, CurrencyDollarIcon, LinkIcon,} from '@heroicons/react/solid'
+import {Box, Button, Card, Flex, Heading, Input, Modal, Progress} from 'rimble-ui';
+import {CalendarIcon, CheckIcon, ChevronDownIcon, CurrencyDollarIcon, LinkIcon, PencilIcon, InformationCircleIcon} from '@heroicons/react/solid'
 import {Menu, Transition} from '@headlessui/react'
 
 function classNames(...classes) {
@@ -53,26 +53,26 @@ export default ({drizzle, drizzleState}) => {
     }, [])
 
     return (
-        <div className="App">
-            <div className="lg:flex lg:items-center lg:justify-between">
-                <div className="flex-1 min-w-0">
-                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-                        Example Charity
-                        <Progress value={(thresholdValue - remainingWeiValue) / thresholdValue} className="ml-4"/>
-                    </h2>
-                    <div className="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
-                        <div className="flex items-center mt-2 text-sm text-gray-500">
-                            <CurrencyDollarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
-                                                aria-hidden="true"/>
-                            Last Donation: {(lastDonationValue / 1e18).toFixed(18)} Ξ
-                        </div>
-                        <div className="flex items-center mt-2 text-sm text-gray-500">
-                            <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true"/>
-                            Closing on January 9, 2020
+        <div className="min-h-screen flex">
+            <div className="m-4 flex flex-col flex-grow justify-between">
+                <div className="lg:flex lg:items-center lg:justify-between">
+                    <div className="flex-1 min-w-0">
+                        <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                            Example Charity
+                        </h2>
+                        <div className="flex flex-col mt-1 sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+                            <div className="flex items-center mt-2 text-sm text-gray-500">
+                                <CurrencyDollarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                                    aria-hidden="true"/>
+                                Last Donation: {(lastDonationValue / 1e18).toFixed(18)} Ξ
+                            </div>
+                            <div className="flex items-center mt-2 text-sm text-gray-500">
+                                <CalendarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true"/>
+                                Closing on January 9, 2020
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="flex mt-5 lg:mt-0 lg:ml-4">
+                    <div className="flex mt-5 lg:mt-0 lg:ml-4">
                     <span className="hidden ml-3 sm:block">
                         <button
                             type="button"
@@ -83,7 +83,7 @@ export default ({drizzle, drizzleState}) => {
                         </button>
                     </span>
 
-                    <span className="sm:ml-3">
+                        <span className="sm:ml-3">
                         <button
                             type="button"
                             className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
@@ -94,51 +94,62 @@ export default ({drizzle, drizzleState}) => {
                         </button>
                     </span>
 
-                    {/* Dropdown */}
-                    <Menu as="span" className="relative ml-3 sm:hidden">
-                        {({open}) => (
-                            <>
-                                <Menu.Button
-                                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                    More
-                                    <ChevronDownIcon className="w-5 h-5 ml-2 -mr-1 text-gray-500" aria-hidden="true"/>
-                                </Menu.Button>
+                        {/* Dropdown */}
+                        <Menu as="span" className="relative ml-3 sm:hidden">
+                            {({open}) => (
+                                <>
+                                    <Menu.Button
+                                        className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                        More
+                                        <ChevronDownIcon className="w-5 h-5 ml-2 -mr-1 text-gray-500" aria-hidden="true"/>
+                                    </Menu.Button>
 
-                                <Transition
-                                    show={open}
-                                    as={Fragment}
-                                    enter="transition ease-out duration-200"
-                                    enterFrom="transform opacity-0 scale-95"
-                                    enterTo="transform opacity-100 scale-100"
-                                    leave="transition ease-in duration-75"
-                                    leaveFrom="transform opacity-100 scale-100"
-                                    leaveTo="transform opacity-0 scale-95"
-                                >
-                                    <Menu.Items
-                                        static
-                                        className="absolute right-0 w-48 py-1 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                    <Transition
+                                        show={open}
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
                                     >
-                                        <Menu.Item>
-                                            {({active}) => (
-                                                <a
-                                                    href="#"
-                                                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                >
-                                                    View Contract
-                                                </a>
-                                            )}
-                                        </Menu.Item>
-                                    </Menu.Items>
-                                </Transition>
-                            </>
-                        )}
-                    </Menu>
+                                        <Menu.Items
+                                            static
+                                            className="absolute right-0 w-48 py-1 mt-2 -mr-1 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                        >
+                                            <Menu.Item>
+                                                {({active}) => (
+                                                    <a
+                                                        href="#"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                    >
+                                                        View Contract
+                                                    </a>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </>
+                            )}
+                        </Menu>
+                    </div>
                 </div>
-            </div>
-            <Card className="section">
-                <Heading as={"h1"}>{messageValue}</Heading>
-                <Text fontWeight={"bold"}>Last Donor: </Text>
-                <EthAddress address={lastDonorValue}/>
+                <div className={"text-center"}>
+                    <p className="text-5xl place-self-center">{messageValue}</p>
+                    <div className={"flex flex-row space-x-2 justify-center"}>
+                        <div className="flex items-center mt-2 text-sm text-gray-500">
+                            <CurrencyDollarIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                                aria-hidden="true"/>
+                            Last Donation: {(lastDonationValue / 1e18).toFixed(18)} Ξ
+                        </div>
+                        <div className="flex items-center mt-2 text-sm text-gray-500">
+                            <PencilIcon className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                                        aria-hidden="true"/>
+                            Left By: {lastDonorValue}
+                        </div>
+                    </div>
+                </div>
                 <Modal isOpen={isOpen}>
                     <Card width={"420px"} p={0}>
                         <Button.Text
@@ -173,7 +184,15 @@ export default ({drizzle, drizzleState}) => {
                         </Flex>
                     </Card>
                 </Modal>
-            </Card>
+                <div className={"flex items-stretch flex-col"}>
+                    <div className="flex justify-center items-center mb-2 text-sm text-gray-500">
+                        {(thresholdValue - remainingWeiValue)/thresholdValue}% Complete
+                        <InformationCircleIcon className="flex-shrink-0 ml-1.5 h-5 w-5 text-gray-400"
+                                               aria-hidden="true"/>
+                    </div>
+                    <Progress value={(thresholdValue - remainingWeiValue) / thresholdValue} className="w-full"/>
+                </div>
+            </div>
         </div>
     );
 };
